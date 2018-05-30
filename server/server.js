322ff464982632ed9e09a404b0891a8f17116b07
Alon Galperin <alonga90@gmail.com>
2018-05-30 15:32:41 +0200

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -12,13 +12,23 @@ app.use(express.static(path.join(__dirname,'../public')));
 
 io.on('connection', (socket) => {
     console.log('New user connected');
-
+    
+    socket.emit('newMessage', {
+        from: 'admin',
+        text: 'welcome to chat'
+    });
+    
+    socket.broadcast.emit('newMessage', {
+       from: 'admin',
+        text: 'newMember'
+    });
+    
     socket.on('createMessage', (message) => {
         console.log('createMessage', message);
-        io.emit('newMessage', {
-           from: message.from,
-           text: message.text,
-           createdAt: new Date().getTime()
+        socket.broadcast.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createAt: new Date().getTime()
         });
     });
 
