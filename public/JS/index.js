b87f208006b9a45aeb81cdde8bbc7390bfde9cca
alongalperin <alonga90@gmail.com>
2018-06-26 13:56:42 +0200

    Add geolocation via geolocation api

--- public/JS/index.js
+++ public/JS/index.js
@@ -16,6 +16,16 @@ socket.on('newMessage', function(message) {
     $("#messages").append(li);
 });
 
+socket.on('newLocationMessage', function(message) {
+    let li = jQuery('<li></li>');
+    var a = jQuery('<a target="_blank">My Current Location</a>');
+
+    li.text(`${message.from}: `);
+    a.attr('href', message.url);
+    li.append(a);
+    $("#messages").append(li);    
+});
+
 $('#message-form').on('submit', function(e) {
     e.preventDefault();
     
@@ -24,5 +34,21 @@ $('#message-form').on('submit', function(e) {
         text: $('[name=message]').val()
     }, function() {
         
+    });
+});
+
+var locationButton = $("#send-location"); // pointer to the send location button
+locationButton.on('click', function() {
+    if (!navigator.geolocation) {
+        return alert('Geolocation not supported by your browser.');
+    }
+
+    navigator.geolocation.getCurrentPosition(function(position) {
+        socket.emit('createLocationMessage', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        });
+    }, function() {
+        alert('Unable to fetch location');
     });
 });
--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const http = require('http');
 const socketIO = require('socket.io');
 
 const {generateMessage} = require('./utils/message');
+const {generateLocationMessage} = require('./utils/message');
 
 const port = process.env.PORT || 3000;
 const app = express();
@@ -23,6 +24,10 @@ io.on('connection', (socket) => {
         console.log('createMessage', message);
         io.emit('newMessage', generateMessage(message.from, message.text));
         callback('This is from the server');
+    });
+
+    socket.on('createLocationMessage', (coords, callback) => {
+        io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
     });
 
     socket.on('disconnect', () => {
